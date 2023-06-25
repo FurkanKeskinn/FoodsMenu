6c0fd6881beabfc2103f0a0eabe87c2be597bad4
Furkan Keskin <ksknmhmt3131@gmail.com>
2023-06-25 18:09:32 +0300

    Initial Commit

---
+++ FoodsMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodsMenu
+//
+//  Created by Furkan on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
